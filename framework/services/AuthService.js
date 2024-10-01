fa5eabe7a666f9023c8f1696b150a861f6dc7aaf
Natalya.Sharapova <natalya.n.sharapova@gmail.com>
2024-10-01 21:16:02 +0400

    fixed eslient error

--- framework/services/AuthService.js
+++ framework/services/AuthService.js
@@ -1,12 +1,7 @@
 import axios from 'axios'
-
-
 export const GenerateToken = async  (userName, password) => {
     return  await axios.post('https://bookstore.demoqa.com/Account/v1/GenerateToken',{
         userName,
         password
     })
-
 }
-
-//export default {GenerateToken}
--- framework/services/UserService.js
+++ framework/services/UserService.js
@@ -11,5 +11,3 @@ export const DeleteUser = async  (userfordeleteID) => {
         userfordeleteID
     })
 }
-
-//export default {GenerateToken}
